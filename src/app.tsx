727e18ac0701543c0928c94840565ba87343a2cb
Sfl <942368681@qq.com>
2021-02-02 16:41:31 +0800

    子应用全局挂载APP_NAME

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,15 @@
 export const qiankun = {
-    // 应用加载之前
-    async bootstrap(props: any) {
-        console.log('subapp bootstrap', props);
-    },
-    // 应用 render 之前触发
-    async mount(props: any) {
-        console.log('subapp mount', props);
-    },
-    // 应用卸载之后触发
-    async unmount(props: any) {
-        console.log('subapp unmount', props);
-    },
+  // 应用加载之前
+  async bootstrap(props: any) {
+    console.log('subapp bootstrap', props);
+    window.APP_NAME = props && props.appName ? props.appName : '';
+  },
+  // 应用 render 之前触发
+  async mount(props: any) {
+    console.log('subapp mount', props);
+  },
+  // 应用卸载之后触发
+  async unmount(props: any) {
+    console.log('subapp unmount', props);
+  },
 };
